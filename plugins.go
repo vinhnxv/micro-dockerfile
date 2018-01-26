15f8afaf244b96c5f259004b8fd373366e31c7b9
Eduardo Alves <ea@arex.io>
2018-01-26 09:12:37 +0000

    enhanc: add k8s registry, remove nats registry, remove jsonrpc2 codec

--- plugins.go
+++ plugins.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	_ "github.com/micro/go-plugins/broker/nats"
-	_ "github.com/micro/go-plugins/codec/jsonrpc2"
-	_ "github.com/micro/go-plugins/registry/nats"
+	_ "github.com/micro/go-plugins/registry/kubernetes"
+	_ "github.com/micro/go-plugins/selector/cache"
 	_ "github.com/micro/go-plugins/transport/nats"
 )
